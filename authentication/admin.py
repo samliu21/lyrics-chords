7b6bd010aa969f3e0c7fbf3c333452a6e20a91cf
samliu21 <sam4button@gmail.com>
2021-08-03 10:00:02 -0400

    Registered admin for authentication models

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import User, Image
 
-admin.site.register(User)
-admin.register(Image)
+class UserAdmin(admin.ModelAdmin):
+	readonly_fields = ('id',)
+
+class ImageAdmin(admin.ModelAdmin):
+	readonly_fields = ('id',)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Image, ImageAdmin)
