bc8b4177fa583dfdd4f7f41a0c5f98e0c1f8b509
samliu21 <sam4button@gmail.com>
2021-08-02 20:09:45 -0400

    Changed Button.module.css to different name

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from "./Button.css";
+import styles from "./ButtonStyle.module.css";
 
 export default function Button(props) {
 	const { className } = props;
